662ff1515d059e999b7d0531363439ad222d5b92
libialany <elibialany@gmail.com>
2025-12-14 15:41:29 -0400

    feat: update executable

--- check-substring.sh
+++ check-substring.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-grep -r --include="*.tfvars" --include="*.tf" "tag*" "$(pwd)"
+s=$(grep -r --include="*.tfvars" --include="*.tf" "tag*" "$(pwd)")
+if [ -z "$s" ]; then
+    echo ":("
+    exit 0
+else
+    echo "wii $s"
+fi
+
